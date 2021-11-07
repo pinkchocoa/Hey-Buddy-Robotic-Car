3ecb9f70d477360357de5661d47db89297f60c25
pinkchocoa <pinkchocoa@gmail.com>
2021-11-08 07:53:55 +0800

    nothing wrong with serial port, something wrong w encoding...

--- serial/serialTest.py
+++ serial/serialTest.py
@@ -5,5 +5,22 @@ ser = serial.Serial(port='/dev/ttyAMA0', baudrate=9600, timeout=1, write_timeout
 
 while (1):
 	response = ser.readlines(None)
+	if response != []:
+		print(response)
+		break
+
 	print(response)
 	time.sleep(2)
+
+print("reading success, now attempt to transmit")
+ser.write(ord('a'))
+
+while (1):
+	
+	response = ser.readlines(None)
+	if response != []:
+		print(response)
+		break
+	print(response)
+	time.sleep(2)
+	ser.write(ord('b'))
