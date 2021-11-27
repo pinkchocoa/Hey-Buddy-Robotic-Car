a90a13ef77602a73f385b31d2943e78c519a7dfe
pinkchocoa <pinkchocoa@gmail.com>
2021-11-27 17:27:14 +0800

    returns coord of blue

---
+++ rpi/camera/camera.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np 
+from picamera.array import PiRGBArray
+from picamera import PiCamera 
+
+def nothing(x):
+    pass
+ 
+# cv2.namedWindow("Trackbars")
+# cv2.createTrackbar("B", "Trackbars", 0, 255, nothing)
+# cv2.createTrackbar("G", "Trackbars", 0, 255, nothing)
+# cv2.createTrackbar("R", "Trackbars", 0, 255, nothing)
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 30
+
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	image = frame.array
+	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+	B = 156#cv2.getTrackbarPos("B", "Trackbars")
+	G = 76#cv2.getTrackbarPos("G", "Trackbars")
+	R = 0#cv2.getTrackbarPos("R", "Trackbars")
+
+	green = np.uint8([[[B, G, R]]])
+	hsvGreen = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+	lowerLimit = np.uint8([hsvGreen[0][0][0]-10,100,100])
+	upperLimit = np.uint8([hsvGreen[0][0][0]+10,255,255])
+
+	mask = cv2.inRange(hsv, lowerLimit, upperLimit)
+
+	coord = cv2.findNonZero(mask)
+	
+	print(coord)
+
+	result = cv2.bitwise_and(image	, image	, mask=mask)
+
+	#cv2.imshow("frame", image)
+	#cv2.imshow("mask", mask)
+	#cv2.imshow("result", result)
+
+	key = cv2.waitKey(1)
+	rawCapture.truncate(0)
+	#if key == 27:
+	#	break
+
+#cv2.destroyAllWindows()
