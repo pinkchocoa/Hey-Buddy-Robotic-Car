3ef260282ea39ec3e19371a3adcb6dfcb5245fa5
aloysiuswjw <46685749+pinkchocoa@users.noreply.github.com>
2021-12-04 04:37:48 +0800

    Update movement.h

--- movement.h
+++ movement.h
@@ -2,13 +2,14 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "ultrasonic.h"
 
+/*these variables are used for the encoder
+to sync up the speed between the two wheels*/
 int detectleft = 0;
 int detectright = 0;
 float ratio = 1;
 
 /* Timer_A PWM Configuration Parameter */
-//this configs for timer A register 1
-//this is tied to P2.4
+//this config for timer A register 1, p2.4
 Timer_A_PWMConfig pwmConfig1 =
 {
         TIMER_A_CLOCKSOURCE_SMCLK,
@@ -19,8 +20,7 @@ Timer_A_PWMConfig pwmConfig1 =
         1000
 };
 
-//this configs for timer A register 1
-//this is tied to P2.5
+//this config for timer A register 2, p2.5
 Timer_A_PWMConfig pwmConfig2 =
 {
         TIMER_A_CLOCKSOURCE_SMCLK,
@@ -31,6 +31,7 @@ Timer_A_PWMConfig pwmConfig2 =
         1000
 };
 
+//this function sets the output on motor for it to go forward
 void setOutputOnMotor(){
     GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN5);
     GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN4);
@@ -38,6 +39,7 @@ void setOutputOnMotor(){
     GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN2);
 }
 
+//this function sets the needed ports as output pins for wheel movement
 void setMotorPorts(){
     //motor A
     GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN4);
@@ -48,21 +50,23 @@ void setMotorPorts(){
     GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN2);
     
     //ENA
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2,
+                                                GPIO_PIN4,
+                                                GPIO_PRIMARY_MODULE_FUNCTION);
     
     //ENB
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN5, GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2,
+                                                GPIO_PIN5,
+                                                GPIO_PRIMARY_MODULE_FUNCTION);
 
     setOutputOnMotor();
 }
 
-//balance pwm
+//this function sets interrupt for port 5 and 6, for the wheel encoders
 void setWheelInterupt(){
-    //declare output of encoder(inputted to msp)
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P6, GPIO_PIN4);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P5, GPIO_PIN5);
 
-    /* Enabling interrupts and starting the watchdog timer */
     GPIO_clearInterruptFlag(GPIO_PORT_P6, GPIO_PIN4);
     GPIO_enableInterrupt(GPIO_PORT_P6, GPIO_PIN4);
     GPIO_clearInterruptFlag(GPIO_PORT_P5, GPIO_PIN5);
@@ -75,6 +79,7 @@ void setWheelInterupt(){
     Interrupt_enableMaster();
 }
 
+//this function sets interrupt for port 1, for S1 and S2
 void setS1S2Interrupt(){
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
     GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);
@@ -84,19 +89,18 @@ void setS1S2Interrupt(){
     GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN4);
     GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN4);
 
-    /* Enabling interrupts and starting the watchdog timer */
     Interrupt_enableInterrupt(INT_PORT1);
     Interrupt_enableSleepOnIsrExit();
     Interrupt_enableMaster();
 }
 
-
+//this function generates PWM for timer A, register 1 and 2
 void generatePWN(){
-    /* Configuring Timer_A to have a period of approximately 80ms and an initial duty cycle of 10% of that (1000 ticks)  */
     Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig1);
     Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig2);
 }
 
+//this function toggles the wheels output to change its direction
 void changeDirection(){
     GPIO_toggleOutputOnPin(GPIO_PORT_P4, GPIO_PIN4);
     GPIO_toggleOutputOnPin(GPIO_PORT_P4, GPIO_PIN5);
@@ -106,37 +110,46 @@ void changeDirection(){
     generatePWN();
 }
 
+//this function sets the PWM to 0 to stop the car
 void zeroPWN(){
     pwmConfig1.dutyCycle = pwmConfig2.dutyCycle = 0;
     generatePWN();
 }
 
+//this function moves the car forward
 void startMoving(){
+    //check front ultrasonic to see if moving forward is possible
     if (getHCSR04DistanceFront() > MIN_DISTANCE){
         pwmConfig1.dutyCycle = pwmConfig2.dutyCycle = 5000;
         generatePWN();
     }
-    else zeroPWN();
+    else zeroPWN(); //no space to move in front, stop
 }
 
+//this function turns the car left
 void rotateCarLeft(){
+    //check left ultrasonic to see if turning left is possible
     if (getHCSR04DistanceLeft() > LR_MIN_DISTANCE){
         pwmConfig1.dutyCycle = 6000;
         pwmConfig2.dutyCycle = 4000;
         generatePWN();
     }
-    else zeroPWN();
+    else zeroPWN(); //no space to turn left, stop
 }
 
+//this function turns the car right
 void rotateCarRight(){
+    //check right ultrasonic to see if turning left is possible
     if (getHCSR04DistanceRight() > LR_MIN_DISTANCE){
         pwmConfig1.dutyCycle = 4000;
         pwmConfig2.dutyCycle = 6000;
         generatePWN();
     }
-    else zeroPWN();
+    else zeroPWN(); //no space to turn right, stop
 }
 
+//this function checks all three ultrasonic sensors
+//dependent on the current state of the PWM duty cycles
 void checkUltraSonic(){
     if (pwmConfig1.dutyCycle > 0 && pwmConfig2.dutyCycle > 0){
         if (pwmConfig1.dutyCycle == pwmConfig2.dutyCycle &&
@@ -154,7 +167,7 @@ void checkUltraSonic(){
     }
 }
 
-
+//port 6 interrupt handler to sync the wheels speed
 void PORT6_IRQHandler(void)
 {
     //FOR RIGHT SIDE SLAVE
@@ -173,6 +186,7 @@ void PORT6_IRQHandler(void)
     GPIO_clearInterruptFlag(GPIO_PORT_P6, status);
 }
 
+//port 5 interrupt handler to sync the wheels speed
 void PORT5_IRQHandler(void)
 {
     //FOR LEFTSIDE SLAVE ENCODER
